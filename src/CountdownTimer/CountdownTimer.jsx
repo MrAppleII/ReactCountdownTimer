9481b66d469b649c6f34f4ddf8c3b0b3a9ef2cca
Jesus Garnica <gtechy100@gmail.com>
2019-07-07 16:56:46 -0700

    Update CountdownTimer.jsx
    Added more try catch blocks. Added preparation for incorrectly set Epoch props.

--- src/CountdownTimer/CountdownTimer.jsx
+++ src/CountdownTimer/CountdownTimer.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
+import PropTypes from "prop-types"
 import moment from "moment"
-import styled, {keyframes} from "styled-components"
+import styled, { keyframes } from "styled-components"
 /*
     Display a timer countdown. It requires however Moment.js to use. At the end of the countdown 
     it fires a function. 
@@ -21,31 +22,31 @@ class CountdownTimer extends Component {
     this.onTimerComplete = this.onTimerComplete.bind(this)
   }
   componentDidMount() {
-    const timeFormat="MM DD YYYY, h:mm a"
-    var timeTillDate = moment.unix(this.props.EpochStartTime)
-    // now lets make it UTC and add 24 hours. 
-    timeTillDate = moment.utc(timeTillDate,timeFormat).add(1,'day')
-   
-   console.log(timeTillDate)
-    var now = moment.utc(new Date(), timeFormat)
-    console.log("now",now)
-    const then = timeTillDate
-    if(then.diff(now, "seconds")>0){
-        this.interval = setInterval(() => {
-          
-           
-             now = moment.utc(new Date(), timeFormat)
-           
+    try {
+      if (this.props.EpochStartTime !== -1) {
+        const timeFormat = "MM DD YYYY, h:mm a"
+        var timeTillDate = moment.unix(this.props.EpochStartTime)
+        // now lets make it UTC and add 24 hours.
+        timeTillDate = moment.utc(timeTillDate, timeFormat).add(1, "day")
+
+        console.log(timeTillDate)
+        var now = moment.utc(new Date(), timeFormat)
+        console.log("now", now)
+        const then = timeTillDate
+        if (then.diff(now, "seconds") > 0) {
+          this.interval = setInterval(() => {
+            now = moment.utc(new Date(), timeFormat)
+
             // const dateDiff = b.diff(a, 'days');
-      
+
             const countdown = moment(then - now)
             // Set the days
             const days = then.diff(now, "days")
-      
+
             const hours = moment.duration(then.diff(now)).hours()
             const minutes = moment.duration(then.diff(now)).minutes()
             const seconds = countdown.format("ss")
-           // console.log("diff", moment.duration(then.diff(now)))
+            // console.log("diff", moment.duration(then.diff(now)))
             if (then.diff(now, "seconds") === 0) {
               if (this.state.functionFired !== true) {
                 this.onTimerComplete()
@@ -55,23 +56,36 @@ class CountdownTimer extends Component {
               }
             }
             // If we are out of time then we call this function
-      
+
             this.setState({ days, hours, minutes, seconds })
           }, 1000)
-
-    }else{
-        const days = 0
-        const hours =0
-        const minutes =0
-        const seconds = 0
-        this.setState({ days, hours, minutes, seconds },
-            function(){
-                this.onTimerComplete()
-            }
-            )
-       
+        } else {
+          const days = 0
+          const hours = 0
+          const minutes = 0
+          const seconds = 0
+          this.setState({ days, hours, minutes, seconds }, function() {
+            this.onTimerComplete()
+          })
+        }
+      } else {
+        const days = -1
+        const hours = -1
+        const minutes = -1
+        const seconds = -1
+        this.setState({ days, hours, minutes, seconds })
+        if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+          console.log(
+            "Epoch was not set correctly. Epoch:",
+            this.props.EpochStartTime
+          )
+        }
+      }
+    } catch (e) {
+      if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+        console.log(e)
+      }
     }
-    
   }
   componentWillUnmount() {
     if (this.interval) {
@@ -80,69 +94,52 @@ class CountdownTimer extends Component {
   }
   onTimerComplete = function() {
     if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-        console.log("Out of time!!")
-      }
-   
+      console.log("Out of time!!")
+    }
+
     this.setState({
       functionFired: true,
     })
   }
-  
 
   render() {
     const { days, hours, minutes, seconds } = this.state
     // Mapping the date values to radius values
-    const daysRadius = mapNumber(days, 30, 0, 0, 360)
-    const hoursRadius = mapNumber(hours, 24, 0, 0, 360)
-    const minutesRadius = mapNumber(minutes, 60, 0, 0, 360)
-    const secondsRadius = mapNumber(seconds, 60, 0, 0, 360)
-   
     try {
-      return (hours>0||minutes>0||seconds>0)? 
-        (
-            <Wrapper className="fadeIn">
-             
-              <CountDownWrapper>
-                  <CountdownItem>
-                  
-                    {hours}
-                    <TimeUnit>hours</TimeUnit>
-                  </CountdownItem>
-                    <Separator>
-                        :
-                    </Separator>
-                   
-                  <CountdownItem>
-                    
-                    {minutes}
-                    <TimeUnit>minutes</TimeUnit>
-                  </CountdownItem>
-                  <Separator>
-                        :
-                    </Separator>
-                  <CountdownItem>
-                  
-                    {seconds}
-                    <TimeUnit>seconds</TimeUnit>
-                  </CountdownItem>
-                
-              </CountDownWrapper>
-            </Wrapper>
-          ): hours!==undefined ? 
-            (<Wrapper>
-              <CountDownWrapper>
-                <ExpiredText>
-                    Expired
-                </ExpiredText>
-              </CountDownWrapper>
-          </Wrapper>) : <Wrapper>
+      return hours > 0 || minutes > 0 || seconds > 0 ? (
+        <Wrapper className="fadeIn">
           <CountDownWrapper>
-              <LoadingText>Loading...</LoadingText>
-              </CountDownWrapper>
-          </Wrapper>
-          
-        }   
-          catch (e) {
+            <CountdownItem>
+              {hours}
+              <TimeUnit>hours</TimeUnit>
+            </CountdownItem>
+            <Separator>:</Separator>
+
+            <CountdownItem>
+              {minutes}
+              <TimeUnit>minutes</TimeUnit>
+            </CountdownItem>
+            <Separator>:</Separator>
+            <CountdownItem>
+              {seconds}
+              <TimeUnit>seconds</TimeUnit>
+            </CountdownItem>
+          </CountDownWrapper>
+        </Wrapper>
+      ) : hours !== undefined ? (
+        <Wrapper>
+          <CountDownWrapper>
+            <ExpiredText>Expired</ExpiredText>
+          </CountDownWrapper>
+        </Wrapper>
+      ) : (
+        <Wrapper>
+          <CountDownWrapper>
+            <LoadingText>Loading...</LoadingText>
+          </CountDownWrapper>
+        </Wrapper>
+      )
+    } catch (e) {
       if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
         console.log(e)
       }
@@ -215,7 +212,7 @@ const CountdownItem = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  align-content:space-around;
+  align-content: space-around;
   flex-direction: column;
   line-height: 1em;
   margin: 0px 2px;
@@ -232,23 +229,18 @@ const CountDownWrapper = styled.div`
   position: relative;
 `
 const ExpiredText = styled.span`
- color: #333;
+  color: #333;
   font-size: 16pt;
   font-weight: 400;
   text-transform: uppercase;
-font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-
+  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
 `
 const LoadingText = styled.p`
-visibility:hidden;
-
+  visibility: hidden;
 `
 const Separator = styled.p`
   font-family: -apple-system, BlinkMacSystemFont, sans-serif;
   line-height: 1em;
-
-
-
 `
 const TimeUnit = styled.span`
   color: #333;
@@ -264,7 +256,7 @@ const Wrapper = styled.div`
   margin: 0;
   box-sizing: border-box;
   height: 50px;
-  &.fadeIn{
+  &.fadeIn {
     animation: ${FadeIn} 0.25s;
   }
   font-family: -apple-system, BlinkMacSystemFont, sans-serif;
@@ -277,5 +269,11 @@ const CountDownSVG = styled.svg`
   width: 100px;
   height: 100px;
 `
+CountdownTimer.propTypes = {
+  EpochStartTime: PropTypes.number.isRequired,
+}
+CountdownTimer.defaultProps = {
+  EpochStartTime: -1,
+}
 
 export default CountdownTimer
